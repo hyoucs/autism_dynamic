0a95786e2a1ee30319d84166e5ed8f20f31673fa
Hongyuan <hyoucs>
2017-07-13 14:33:30 -0400

    update

--- src/dips/dipsTest.m
+++ src/dips/dipsTest.m
@@ -26,18 +26,18 @@ else
 end
 clear pw;
 
-% opt.lambda1s = [logspace(2.5, 0, 9) 0]/1000;
-% opt.lambda2s = [0 0.1 0.5 1 2];
+opt.lambda1s = [logspace(2.5, 0, 9) 0]/1000;
+opt.lambda2s = [0 0.1 0.5 1 2];
 
-opt.lambda1s = [0.15];
-opt.lambda2s = [0.1];
+% opt.lambda1s = [0.15];
+% opt.lambda2s = [0.1];
 
 opt.bLinear = 0; 
 opt.alpha = .2; 
 opt.k = 20;
 opt.nFeaUpd = .8; 
 opt.verbose = 1;
-opt.d = 2;
+opt.d = 1;
 
 tStart = tic; 
 m = dipsFold(data, opt, 1); 
