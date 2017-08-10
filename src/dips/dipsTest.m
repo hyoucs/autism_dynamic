81755b2e161ec072495a833cd34e6a99edb9f921
Hongyuan <hyoucs>
2017-08-10 10:05:56 -0400

    update

--- src/dips/dipsTest.m
+++ src/dips/dipsTest.m
@@ -1,7 +1,7 @@
 % function dipsTest(in_file)
 
 	%% -- DISM with brain data
-	in_file = '../../../autism_private/data/correlation/cc200-fg-wcorr-whole-session-pc/dips_data.mat';
+	in_file = '../../../autism_private/data/correlation/cc200-fng-wcorr-whole-session-pc/dips_data.mat';
 	% in_file = '../../../autism_private/data/correlation/cc200-fg-wcorr-WL10TR-pc/dips_data.mat';
 	load(in_file);
 
@@ -52,11 +52,11 @@
 	opt.k = 20;
 	opt.nFeaUpd = .8; 
 	opt.verbose = 1;
-	opt.d = 2;
+	opt.d = 1;
 
 	tStart = tic; 
 	[m, m2] = dipsFold(data, opt, 2); 
 	toc(tStart)
 
-	save('../../../autism_private/data/output4.mat','m','m2','opt');
+	save('../../../autism_private/data/output5.mat','m','m2','opt');
 
