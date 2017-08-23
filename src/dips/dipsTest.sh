4c6fc61d2dab9fe4fc96b418984ee62d494c4b88
Hongyuan <hyoucs>
2017-08-23 10:03:13 -0400

    update

--- src/dips/dipsTest.sh
+++ src/dips/dipsTest.sh
@@ -2,11 +2,11 @@
 
 j=0
 nb_concurrent_processes=10
-for idxFold in {3..10}
+for idxFold in {3..5}
 do
 
 	echo "    ====== START: DIPS cv fold: ${idxFold} ======"
-	matlab -nodesktop -nosplash -nojvm -r "dipsTest(${idxFold}); exit;"
+	matlab -nodesktop -r "dipsTest(${idxFold}); exit;" &
 	((++j == nb_concurrent_processes)) && { j=0; wait; }
 	echo "    ====== END: DIPS cv fold: ${idxFold} ======"
 
